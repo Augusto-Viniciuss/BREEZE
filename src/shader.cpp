7f4c5ef413cb0515e0948a3daceafbbbde74f28c
Laura de Faria Maranhão Ayres <lauradfma@gmail.com>
2024-10-14 01:11:46 -0300

    Adição de comentários

--- src/shader.cpp
+++ src/shader.cpp
@@ -9,7 +9,7 @@ namespace BREEZE
 {
 Shader::Shader(const char* filename, enum shader_type shader_type) : filename(filename), type(shader_type) {
 }
-
+// Lê o conteúdo do arquivo shader
 std::string Shader::read_file(const std::string& filename) {
   std::string   contents;
   std::ifstream shader_file(filename);
@@ -22,11 +22,12 @@ std::string Shader::read_file(const std::string& filename) {
 
   return contents;
 }
-
+// Compila o shader
 bool Shader::compile() {
 
   std::string contents = read_file(this->filename);
   GLenum      gl_shader_type;
+  // Define o tipo de shader OpenGL com base no tipo especificado
   switch (type) {
     case vertex:
       gl_shader_type = GL_VERTEX_SHADER;
@@ -35,7 +36,7 @@ bool Shader::compile() {
       gl_shader_type = GL_FRAGMENT_SHADER;
       break;
   }
-
+  // Cria o shader no OpenGL
   this->shader_id           = glCreateShader(gl_shader_type);
   const char* source_string = contents.c_str();
   glShaderSource(this->shader_id, 1, &source_string, nullptr);
@@ -44,7 +45,7 @@ bool Shader::compile() {
   char log[512];
   int  success;
   glGetShaderiv(this->shader_id, GL_COMPILE_STATUS, &success);
-
+  // Verifica se houve erro na compilação
   if (!success) {
     glGetShaderInfoLog(this->shader_id, 512, nullptr, log);
     std::cerr << this->filename << ": Shader compilation failed -> " << log << std::endl;
@@ -53,11 +54,11 @@ bool Shader::compile() {
 
   return true;
 }
-
+// Destrutor da classe Shader
 Shader::~Shader() {
   glDeleteShader(this->shader_id);
 }
-
+// Obtém o ID do shader
 uint32_t Shader::get_shader_id() const {
   return this->shader_id;
 }
