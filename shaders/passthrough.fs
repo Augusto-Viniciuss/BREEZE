a47b2b8adb6afb936be8f317f985e322ba026dd6
Laura de Faria Maranhão Ayres <lauradfma@gmail.com>
2024-10-14 01:37:49 -0300

    Adição de comentários

--- shaders/passthrough.fs
+++ shaders/passthrough.fs
@@ -1,10 +1,13 @@
 #version 460 core
+// Saída do fragment shader: cor do fragmento
 out vec4 FragColor;
-in vec2 tex_coords;
-in vec3 normal;
+in vec2 tex_coords;    // Coordenadas de textura do vértice
+in vec3 normal;        // Vetor normal do vértice (não utilizado neste shader)
 
+// Contém a textura a ser aplicada
 uniform sampler2D current_texture;
 void main()
 {
+    // Aplica a textura usando as coordenadas de textura e define a cor do fragmento
     FragColor = vec4(texture(current_texture, tex_coords).rgb, 1.0);
 }
