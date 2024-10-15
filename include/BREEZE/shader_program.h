7fd2ca623076ccf0192b26ea041ededf419c3618
Laura de Faria Maranhão Ayres <lauradfma@gmail.com>
2024-10-14 22:26:17 -0300

    Adição de comentários

--- include/BREEZE/shader_program.h
+++ include/BREEZE/shader_program.h
@@ -6,14 +6,22 @@
 
 namespace BREEZE
 {
+// Um programa de shader é composto por dois shaders principais: vertex shader e fragment shader
 class ShaderProgram {
 private:
-  uint32_t id;
+  uint32_t id; // ID do programa de shaders no OpenGL
 
 public:
+  // Construtor que recebe dois shaders (vertex e fragment) e os liga para formar um programa de shader
   explicit ShaderProgram(Shader* vShader, Shader* fShader);
+
+  // Retorna o ID do programa de shaders, que é gerado pelo OpenGL
   uint32_t get_id() const;
-  void     use();
+
+  // Ativa o programa de shaders para ser usado na renderização
+  void use();
+
+  // Destrutor que libera os recursos associados ao programa de shaders
   ~ShaderProgram();
 };
 }  // namespace BREEZE
