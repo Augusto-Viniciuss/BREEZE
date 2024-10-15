a3a4c5e7f2560fff4c86c4474a06548524bad94e
Laura de Faria Maranhão Ayres <lauradfma@gmail.com>
2024-10-14 22:27:31 -0300

    Adição de comentários

--- include/BREEZE/texture.h
+++ include/BREEZE/texture.h
@@ -5,15 +5,25 @@
 
 namespace BREEZE
 {
+// As texturas são usadas para mapear imagens em superfícies 3D durante o processo de renderização
 class Texture {
 private:
-  uint32_t texture_id;
+  uint32_t texture_id;  // ID da textura gerado pelo OpenGL
 
 public:
+  // Construtor que recebe o caminho de um arquivo de imagem para carregar como textura
   explicit Texture(const char* filename);
-  bool     load(const char* filename);
-  void     use();
+
+  // Carrega uma textura a partir de um arquivo de imagem e armazena no contexto OpenGL
+  bool load(const char* filename);
+
+  // Ativa a textura para ser usada no processo de renderização
+  void use();
+
+  // Retorna o ID da textura, utilizado para referenciar a textura no OpenGL
   uint32_t get_id() const;
+
+  // Destrutor que libera os recursos alocados para a textura
   ~Texture();
 };
 }  // namespace BREEZE
